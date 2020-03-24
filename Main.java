60298683173362250b9b7f53ace6cf603e240049
sirbu99 <44173240+sirbu99@users.noreply.github.com>
2020-03-24 19:26:41 +0200

    Laborator 6 - partea obligatorie

--- Main.java
+++ Main.java
@@ -1,30 +1,8 @@
 package com.company;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 public class Main {
-    public static void main(String args[]) throws InvalidCatalogException, IOException, URISyntaxException {
-        Main app = new Main();
-        app.testCreateSave();
-        app.testLoadView();
-    }
 
-    private void testCreateSave() throws IOException {
-        Catalog catalog =
-                new Catalog("Java Resources", "d://java//catalog.ser");
-        Document doc = new Document("java1", "Java Course 1",
-                "https://profs.info.uaic.ro//~acf//java//slides//en//intro_slide_en.pdf");
-        doc.addTag("type", "Slides");
-        catalog.add(doc);
-        CatalogUtil.save(catalog);
-    }
-
-    private void testLoadView() throws IOException, InvalidCatalogException, URISyntaxException {
-        Catalog catalog= CatalogUtil.load("d://java//catalog.ser");
-        assert catalog != null;
-        Document doc = catalog.findById("java1");
-        CatalogUtil.view(doc);
+    public static void main(String[] args) {
+        new MainFrame().setVisible(true);
     }
 }
